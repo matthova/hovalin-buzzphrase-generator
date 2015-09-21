0d612727e14089d248a43ce6065f9ce9f838ddc8
Matt Hova <matthova87@gmail.com>
2015-09-20 20:12:13 -0700

    Update hovalin-buzzphrase-generator.js

--- hovalin-buzzphrase-generator.js
+++ hovalin-buzzphrase-generator.js
@@ -7,12 +7,12 @@ $( document ).ready(function() {
 });
 
 var quotes = [
+	"that can do your taxes",
 	"that you've heard about this week",
-	"that you told your mom about",
+	"that your mom told you about",
 	"that can cat-sit while you're on vacation",
 	"programmed to remember your birthday",
 	"to travel to Mars",
-	"that can do your taxes",
 	"that knows how to parallel park",
 	"that can give your kids, 'the talk'",
 	"/ canoe paddle"
